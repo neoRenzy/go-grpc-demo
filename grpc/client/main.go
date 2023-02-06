583232531f29c01787e2b28a8170f8a6a83cd250
renzheyi <renzheyi@360.cn>
2023-02-06 16:00:15 +0800

    添加server和client的demo

--- grpc/client/main.go
+++ grpc/client/main.go
@@ -1,4 +1,23 @@
 package client
 
+import (
+	"context"
+	"go-grpc-demo/grpc/pb"
+	"google.golang.org/grpc"
+	"log"
+)
+
 func main() {
+	conn, err := grpc.Dial("127.0.0.1:8002")
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewMessageSenderClient(conn)
+	resp, err := client.Send(context.Background(), &pb.MessageRequest{SaySomething: "hello world!"})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Println("receive message:", resp.GetResponseSomething())
 }
--- grpc/server/main.go
+++ grpc/server/main.go
@@ -1,1 +1,34 @@
 package server
+
+import (
+	"context"
+	"go-grpc-demo/grpc/pb"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+type MessageSenderServerImpl struct {
+	*pb.UnimplementedMessageSenderServer
+}
+
+func (MessageSenderServerImpl) Send(context context.Context, request *pb.MessageRequest) (*pb.MessageResponse, error) {
+	log.Println("receive message:", request.GetSaySomething())
+	resp := &pb.MessageResponse{}
+	resp.ResponseSomething = "roger that!"
+	return resp, nil
+}
+
+func main() {
+	srv := grpc.NewServer()
+	pb.RegisterMessageSenderServer(srv, MessageSenderServerImpl{})
+	listener, err := net.Listen("tcp", ":8002")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	err = srv.Serve(listener)
+	if err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
