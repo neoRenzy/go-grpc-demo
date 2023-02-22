89bc383da6258d6d7f3c7dd0ed4669ba8f12a4f8
renzheyi <renzheyi@360.cn>
2023-02-22 16:44:42 +0800

    server

--- grpc/server/server.go
+++ grpc/server/server.go
@@ -11,8 +11,18 @@ import (
 )
 
 func main() {
-	// 单线程处理任务，port:8080
-	oneThreadHandler()
+	// port:8080
+	srv := grpc.NewServer()
+	pb.RegisterMessageSenderServer(srv, MessageSenderServerImpl{})
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	err = srv.Serve(listener)
+	if err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 
 	fmt.Println("server start")
 }
@@ -33,18 +43,4 @@ func (MessageSenderServerImpl) Send(context context.Context, request *pb.Message
 		Result: firstNum ^ secondNum,
 	}
 	return resp, nil
-}
-
-func oneThreadHandler() {
-	srv := grpc.NewServer()
-	pb.RegisterMessageSenderServer(srv, MessageSenderServerImpl{})
-	listener, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-
-	err = srv.Serve(listener)
-	if err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
 }
